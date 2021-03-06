b0c746e450c957d81dee6274534d9de12113b6da
aostrovska <anastasija.ostrovska25@gmail.com>
2021-03-06 12:09:36 +0200

    Firts try to add base(school)

--- client/client.go
+++ client/client.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"net"
 	"fmt"
+	"net"
 )
 
 func main() {
@@ -16,7 +16,16 @@ func main() {
 	fmt.Scan(&s)
 
 	conn.Write([]byte(s))
-
+	switch s {
+	case "delete":
+		var id int
+		fmt.Scan(&id)
+		conn.Write(id)
+	case "add":
+		var person interface{}
+		fmt.Scan(&person)
+		conn.Write(person)
+	}
 	buf := make([]byte, 2000)
 
 	n, err := conn.Read(buf)
--- server/server.go
+++ server/server.go
@@ -1,17 +1,114 @@
 package main
 
 import (
-	"net"
 	"fmt"
+	"net"
 )
 
 type Data struct {
-	N string
+	N  string
 	Ch chan int
 }
 
+type IdData struct {
+	id int
+}
+
+type ObjData struct {
+	obj interface{}
+}
+
+type Teacher struct {
+	Firstname string
+	Lastname  string
+	Id        int
+	Subject   string
+	Classroom int
+}
+
+type Student struct {
+	Firstname string
+	Lastname  string
+	Id        int
+	Class     int
+}
+
+type Stuff struct {
+	Firstname string
+	Lastname  string
+	Id        int
+	Phone     string
+	Classroom int
+}
+
+type School interface {
+	PrintAll()
+	GetId() int
+}
+
+var school []School
 var dat Data
 
+func Add(s *[]School, per School) {
+	*s = append(*s, per)
+}
+
+func Remove(s []School, id int) {
+	for i := 0; i < len(s); i++ {
+		if s[i].GetId() == id {
+			s[i] = nil
+		}
+	}
+}
+
+func (t *Teacher) GetId() int {
+	return t.Id
+}
+
+func (stud *Student) GetId() int {
+	return stud.Id
+}
+
+func (st *Stuff) GetId() int {
+	return st.Id
+}
+
+func listAll(s []School) {
+	for i := 0; i < len(s); i++ {
+		if s[i] != nil {
+			s[i].PrintAll()
+		}
+	}
+}
+func (st *Stuff) PrintAll() {
+	fmt.Println("Stuff : ", st.Firstname, st.Lastname, st.Id, st.Phone, st.Classroom)
+}
+func (stud *Student) PrintAll() {
+	fmt.Println("Student : ", stud.Firstname, stud.Lastname, stud.Id, stud.Class)
+}
+
+func (t *Teacher) PrintAll() {
+	fmt.Println("Teacher : ", t.Firstname, t.Lastname, t.Id, t.Subject, t.Classroom)
+}
+
+func ListStud(s []School) {
+	for i := 0; i < len(s); i++ {
+		switch s[i].(type) {
+		case *Student:
+			s[i].PrintAll()
+		}
+	}
+}
+
+func ListTeacher(s []School) {
+	for i := 0; i < len(s); i++ {
+		switch s[i].(type) {
+		case *Teacher:
+			s[i].PrintAll()
+
+		}
+	}
+}
 func main() {
 	l, err := net.Listen("tcp", "127.0.0.1:12667")
 	if err != nil {
@@ -41,13 +138,36 @@ func HandleConnection(conn net.Conn) {
 		return
 	}
 
-	<- dat.Ch
+	<-dat.Ch
 
 	fmt.Println(string(buf[:n]))
 	dat.N = string(buf[:n])
-
-	data := []byte("Connection great")
-	conn.Write(data)
+	switch dat.N {
+	case "delete":
+		data := []byte("Input ID : ")
+		conn.Write(data)
+		var IdDat IdData
+		fmt.Println(string(buf[:n]))
+		IdDat.id = int(buf[:n])
+		Remove(school, IdDat.id)
+	case "add":
+		data = []byte("Input person : ")
+		conn.Write(data)
+		var ObjDat ObjData
+		fmt.Println(string(buf[:n]))
+		ObjDat.obj = interface{}(buf[:n])
+		Add(school, ObjDat.obj)
+	case "list all":
+		listAll(school)
+	case "list students":
+		ListStud(school)
+	case "list teachers":
+		ListTeacher(school)
+	default:
+		fmt.Println("Incorect input ")
+	}
+	/*	data := []byte("Connection great")
+		conn.Write(data)*/
 
 	dat.Ch <- 1
 
